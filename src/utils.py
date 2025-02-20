47016c0e514ecc652dc78f71fb3a89c12f3757cb
EgorovNikolay <egorovnikolay14021994@gmail.com>
2025-02-20 18:33:36 +0300

    done homework14.1

---
+++ src/utils.py
@@ -0,0 +1,19 @@
+from src.product import Product
+from src.category import Category
+import json
+
+
+def load_data_from_json(file_path: str):
+    """Чтение данных из json - файла и получение из файла данных для экземпляров класса"""
+    with open(file_path, 'r', encoding='utf-8') as file:
+        data = json.load(file)
+        categories = []
+        for category_data in data:
+            products = [Product(**product) for product in category_data["products"]]
+            category = Category(
+                name=category_data["name"],
+                description=category_data["description"],
+                products=products
+            )
+            categories.append(category)
+        return categories
---
+++ tests/test_utils.py
@@ -0,0 +1,14 @@
+from src.category import Category
+from src.utils import load_data_from_json
+from unittest.mock import mock_open, patch
+
+def test_load_data_from_json(sample_json_data):
+    with patch("builtins.open", mock_open(read_data=sample_json_data)):
+        categories = load_data_from_json("mock.json")
+        assert len(categories) == 2
+        assert categories[0].name == "Смартфоны"
+        assert len(categories[0].products) == 3
+        assert categories[1].name == "Телевизоры"
+        assert len(categories[1].products) == 1
+        assert Category.category_count >= 2
+        assert Category.product_count >= 4
