35ea080a59754871a652c935c47096ab25c03850
EgorovNikolay <egorovnikolay14021994@gmail.com>
2025-03-10 19:51:45 +0300

    done homework17.1

--- src/product.py
+++ src/product.py
@@ -3,6 +3,8 @@ from src.log_mixin import LogMixin
 
 class Product(BaseProduct, LogMixin):
     def __init__(self, name, description, price, quantity):
+        if quantity == 0:
+            raise ValueError("Товар с нулевым количеством не может быть добавлен")
         super().__init__( name, description, quantity)
         self.__price = price
 
